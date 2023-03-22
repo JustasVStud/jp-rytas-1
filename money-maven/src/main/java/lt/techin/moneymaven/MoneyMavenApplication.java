c120b4efaf06283e14131e7834dade653a9402d6
Justas Vaitkevicius <Justas.Vaitkevicius@stud.vtmc.lt>
2023-03-22 19:23:48 +0200

    initial frameworks

---
+++ money-maven/src/main/java/lt/techin/moneymaven/MoneyMavenApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.moneymaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMavenApplication.class, args);
+	}
+
+}
---
+++ money-maven/src/test/java/lt/techin/moneymaven/MoneyMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.moneymaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
