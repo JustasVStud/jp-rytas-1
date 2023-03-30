16025c7d1cb8014a0c4c702a6b67932a6b8f7651
Justas Vaitkevicius <Justas.Vaitkevicius@stud.vtmc.lt>
2023-03-30 10:28:15 +0300

    application.properties now have mysql config settings to connect to project db added user getter for connectivity testing

---
+++ money-maven/src/main/java/lt/techin/moneymaven/model/User.java
@@ -0,0 +1,97 @@
+package lt.techin.moneymaven.model;
+
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+	@Column(name = "username", nullable = false)
+	private String username;
+	@Column(name= "password", nullable = false)
+	private String password;
+	@Column(name="is_admin" , nullable = false, columnDefinition = "BOOLEAN")
+	private boolean isAdmin;
+	
+	public User() {
+		super();
+	}
+	
+	public User(Integer userId, String username, String password, boolean isAdmin) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.isAdmin = isAdmin;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(isAdmin, password, userId, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return isAdmin == other.isAdmin && Objects.equals(password, other.password)
+				&& Objects.equals(userId, other.userId) && Objects.equals(username, other.username);
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", isAdmin=" + isAdmin
+				+ "]";
+	}
+	
+	
+	
+}
