c58d51e5f7b126577fee1d6f0c2a1603fdbf14b8
Justas Vaitkevicius <Justas.Vaitkevicius@stud.vtmc.lt>
2023-04-05 11:23:54 +0300

    pre branch merge

--- money-maven-web/src/components/IncomeTable.jsx
+++ money-maven-web/src/components/IncomeTable.jsx
@@ -1,7 +1,23 @@
+import {useState, useEffect} from 'react'; 
+import axios from "axios";
+import { Table } from "react-bootstrap";
+
 function IncomeTable() {
-    return ( <div>
-        income tablle todo
-    </div> );
+
+    //const [incomes, setIncomes] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get('http://localhost:8080/api/incomes/')
+        .then(response => console.log(response))
+    });
+
+    return ( 
+    <>
+        <Table>
+            
+        </Table>
+    </> );
 }
 
 export default IncomeTable;
