232a2102b3e17e57b2a263f93fd3e077d381ba9c
Egidijus Jakavonis <e.jakavonis@gmail.com>
2023-04-06 14:32:54 +0300

    IncomeFormConnect

--- money-maven-web/src/components/IncomeForm.jsx
+++ money-maven-web/src/components/IncomeForm.jsx
@@ -1,35 +1,108 @@
-import React from 'react'
+import { Formik } from "formik";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import {useEffect, useState} from "react";
+
+
+const baseUrl = "http://localhost:8080/api/incomes/";
+
 
 function IncomeForm() {
+
+  let { id } = useParams();
+  let [existingIncome, setExistingIncome] = useState({
+    incomeAmount: 0.0,
+    incomeDescription: "",
+    incomeDatetime: "",
+  });
+
+  useEffect(() => {
+    axios.get(baseUrl + id)
+      .then((response) => setExistingIncome(response.data))
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  const navigate = useNavigate();
+
   return (
-    <div className="p-5">
-        <h2 className="text-center">Income</h2>
-        <form >
-            <div className="form-group">
-                <label htmlFor="salary">Salary</label>
-                <input type="text" className="form-control" id="salary" aria-describedby="emailHelp"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="interest">Interest</label>
-                <input type="text" className="form-control" id="interest"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="investments">Investments</label>
-                <input type="text" className="form-control" id="investments"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="gifts">Gifts</label>
-                <input type="text" className="form-control" id="gifts"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="allowance">Allowance</label>
-                <input type="text" className="form-control" id="allowance"/>
-            </div>
+    <Container>
+      <Row>
+                <h3 className="">Income</h3>
+            </Row>
+            <Row>
+    <Formik
+      initialValues={existingIncome}
+      onSubmit={(values, { resetForm }) => {
+        console.log(values)
+        axios.patch(baseUrl + id, values)
+          .then((response) => {console.log(response.data)
+            resetForm()
+            navigate("/income");
+          })
+          .catch((err) => console.log(err));
+      }}
+      enableReinitialize
+    >
+      {({ values, handleChange, handleBlur, handleSubmit, dirty }) => (
+        <Form onSubmit={handleSubmit} style={{ margin: "100px" }}>
+          <Form.Group className="mb-3">
+            <Form.Label>Amount</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="1000 &#x20AC;"
+              name="incomeAmount"
+              size="sm"
+              value={values.incomeAmount}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Decription</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Source of Income"
+              name="incomeDescription"
+              size="sm"
+              value={values.incomeDescription}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Date and Time</Form.Label>
+            <Form.Control
+              type="datetime-local"
+              placeholder="1900-01-01"
+              name="incomeDatetime"
+              size="sm"
+              value={values.incomeDatetime}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Form.Group>
 
-            <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
-  )
+          <Row>
+            <Col>
+              <Button
+                variant="secondary"
+                type="submit"
+                style={{ marginTop: "10px" }}
+              >
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      )}
+    </Formik>
+    </Row>
+    </Container>
+  );
 }
-
-export default IncomeForm
+export default IncomeForm;
--- money-maven-web/vite.config.js
+++ money-maven-web/vite.config.js
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [react(), eslint()],
   server: {
     port: 3000,
+    formik: 'formik/dist/formik.umd.production.min.js'
   }
 })
