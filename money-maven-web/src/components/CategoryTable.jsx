510fd833ff59d1123601edc4a2af1acad321f899
Egidijus Jakavonis <e.jakavonis@gmail.com>
2023-04-23 13:08:14 +0300

    Pataisyta CategoryTablejsx trukumai

--- money-maven-web/src/components/CategoryTable.jsx
+++ money-maven-web/src/components/CategoryTable.jsx
@@ -1,41 +1,45 @@
 import { useState, useEffect } from 'react';
-import axios from "axios";
-import { Button, Row, Table, Container } from "react-bootstrap";
+import axios from 'axios';
+import { Button, Row, Table, Container } from 'react-bootstrap';
 import NoElementsTableRow from './NoElementsTableRow';
 import Category from './Category';
 import { Link } from 'react-router-dom';
 
 function CategoryTable() {
   const [expenseTypes, setExpenseTypes] = useState([]);
+  const [deleteExpenseTypes, setDeleteExpenseTypes] = useState([]);
 
   useEffect(() => {
     console.log('trigger');
     axios
       .get('http://localhost:8080/api/expenseTypes')
-      .then(response => setExpenseTypes(response.data))
-      .catch((err) => console.log(err))
-  }, []);
+      .then((response) => setExpenseTypes(response.data))
+      .catch((err) => console.log(err));
+  }, [deleteExpenseTypes]);
 
-  function handleDelete(id) {
-    setExpenseTypes(expenseTypes.filter(expenseType => expenseType.typeId !== id));
-  }
-
-  let CategoryJSX;
+  let categoryjsx;
   if (expenseTypes.length > 0) {
-    CategoryJSX = expenseTypes.map((expenseType) => {
-      return (<Category expenseType={expenseType} onDelete={handleDelete} key={expenseType.typeId} />)
+    categoryjsx = expenseTypes.map((expenseType) => {
+      return (
+        <Category
+          expenseType={expenseType}
+          onDelete={() => setDeleteExpenseTypes(Date.now())}
+          key={expenseType.typeId}
+        />
+      );
     });
   } else {
-    CategoryJSX = <NoElementsTableRow elementAddCategory={"ExpenseTypes"} />;
+    categoryjsx = <NoElementsTableRow elementAddCategory={'ExpenseTypes'} />;
   }
 
   return (
     <Container className="form-style">
       <Row className="form-buttons-container">
-        <Link to={"/category/add/"}>
+        <Link to={'/category/add/'}>
           <Button variant="primary">Create</Button>
         </Link>
       </Row>
+
       <Table>
         <thead>
           <tr>
@@ -44,9 +48,7 @@ function CategoryTable() {
             <th>Delete</th>
           </tr>
         </thead>
-        <tbody>
-          {CategoryJSX}
-        </tbody>
+        <tbody>{categoryjsx}</tbody>
       </Table>
     </Container>
   );
