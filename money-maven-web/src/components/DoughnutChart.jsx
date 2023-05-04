41a57616377dab126dd490fa70771a72860dc7bd
Egidijus Jakavonis <e.jakavonis@gmail.com>
2023-05-04 13:52:47 +0300

    Pridetas grafikas Doughnut

--- money-maven-web/src/components/DoughnutChart.jsx
+++ money-maven-web/src/components/DoughnutChart.jsx
@@ -12,9 +12,10 @@ import {
 } from "chart.js";
 import DateTimePicker from "react-datetime-picker";
 import { FaCalendarAlt } from "react-icons/fa";
-import { Container, Button, Row, Form, Col } from "react-bootstrap";
+import { Container, Row, Form, Col } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import moment from "moment";
 
 ChartJs.register(
   CategoryScale,
@@ -30,114 +31,75 @@ ChartJs.register(
 function DoughnutChart() {
   const [expenses, setExpenses] = useState([]);
   const [selectedCategory] = useState("");
-  const [selectedMonthFrom, setSelectedMonthFrom] = useState("");
-  const [selectedMonthTo, setSelectedMonthTo] = useState("");
+  const [selectedStartDate] = useState("");
+  const [selectedSetEndDate] = useState("");
   const [deleteExpense] = useState([]);
   const [deleteIncome] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
   useEffect(() => {
     axios
-      // .get(`http://localhost:8080/api/expenses`, {})
-      .get('http://localhost:8080/api/expenses?page=0&pageSize=1000')
-      .then((response) => console.log(response))
-      // .then((response) => setExpenses(response.data))
+      .get("http://localhost:8080/api/expenses?page=0&pageSize=10000", {
+        params: {
+          startDate: startDate || null,
+          endDate: endDate || null,
+        },
+      })
+      .then((response) => setExpenses(response.data.content))
       .catch((err) => console.log(err));
   }, [
     deleteExpense,
     deleteIncome,
-    selectedMonthFrom,
-    selectedMonthTo,
+
     selectedCategory,
+    startDate,
+    endDate,
   ]);
 
-  const categories = Array.from(
-    new Set(expenses.map((expense) => expense.expenseTypeName))
-  );
-  const handleMonthFromChange = (date) => {
-    const month = date.getMonth();
-    setSelectedMonthFrom(month);
+  const categories = expenses
+    ? Array.from(new Set(expenses.map((expense) => expense.expenseTypeName)))
+    : [];
+
+  const handleStartDateChange = (e) => {
+    if (e != null) {
+      setStartDate(moment(e).format("YYYY-MM-DDTHH:mm:ss"));
+    } else {
+      setStartDate(e);
+    }
   };
 
-  const handleMonthToChange = (date) => {
-    const month = date.getMonth();
-    setSelectedMonthTo(month);
+  const handleEndDateChange = (e) => {
+    if (e != null) {
+      setEndDate(moment(e).format("YYYY-MM-DDTHH:mm:ss"));
+    } else {
+      setEndDate(e);
+    }
   };
-
-  const handleFilter = () => {
-    
-    let url = "http://localhost:8080/api/expenses";
-    if (selectedCategory) {
-      url += `?expenseTypeName=${selectedCategory}`;
-    }
-    if (selectedMonthFrom && selectedMonthTo) {
-      url += `${selectedCategory ? "&" : "?"
-      }monthFrom=${selectedMonthFrom}&monthTo=${selectedMonthTo}`;
-    }
-    axios
-      .get(url)
-      .then((response) => setExpenses(response.data))
-      .catch((err) => console.log(err));
-  };
-
-
-  const handleClearMonthFilter = () => {
-    setSelectedMonthFrom("");
-    setSelectedMonthTo("");
-    handleFilter();
-    axios
-      .get("http://localhost:8080/api/expenses")
-      .then((response) => setExpenses(response.data))
-      .catch((err) => console.log(err));
-  };
-
-  const expensesByCategoryAndMonth = expenses.reduce((acc, expense) => { 
-    const month = new Date(expense.expenseDatetime).getMonth();
-    const category = expense.expenseTypeName;
-  
-    if (
-      (selectedMonthFrom && month < selectedMonthFrom) ||
-      (selectedMonthTo && month > selectedMonthTo) ||
-      (selectedCategory && category !== selectedCategory)
-    ) 
-    {
-      return acc;
-    }
-  
-    if (!acc[category]) {
-      acc[category] = {};
-    }
-    if (!acc[category][month]) {
-      acc[category][month] = {
-        total: 0,
-      };
-    }
-    acc[category][month].total += expense.expenseAmount;
-  
-    return acc;
-  }, {});
 
   const dataDoughnut = {
-    labels:  categories,
+    labels: categories,
     datapoints: "1,2,3",
     datasets: [
       {
         label: "Expenses by category",
         data:
-         categories.map((category) =>
-          expenses
-            .filter(
-              (expense) =>
-                expense.expenseTypeName === category &&
-                (!selectedMonthFrom ||
-                  new Date(expense.expenseDatetime) >=
-                    new Date(selectedMonthFrom)) &&
-                (!selectedMonthTo ||
-                  new Date(expense.expenseDatetime) <=
-                    new Date(selectedMonthTo))
-            )
-            .reduce((acc, expense) => acc + expense.expenseAmount, 0),
-        ),
-        
+          categories &&
+          categories.map((category) =>
+            expenses
+              .filter(
+                (expense) =>
+                  expense.expenseTypeName === category &&
+                  (!selectedStartDate ||
+                    new Date(expense.expenseDatetime) >=
+                      new Date(selectedStartDate)) &&
+                  (!selectedSetEndDate ||
+                    new Date(expense.expenseDatetime) <=
+                      new Date(selectedSetEndDate))
+              )
+              .reduce((acc, expense) => acc + expense.expenseAmount, 0)
+          ),
+
         backgroundColor: [
           "#ffcd56",
           "#ff6384",
@@ -180,55 +142,53 @@ function DoughnutChart() {
   };
 
   return (
-<Container>
-<Row className='table-filter'>
-<Col className='table-filter--size'>
-<Form.Label>Doughnut Chart</Form.Label>
-<Row className="form-buttons-container">
-<Form.Group>
-<Form.Label>Date from</Form.Label>
-<DateTimePicker
-value={selectedMonthFrom ? new Date(2023, selectedMonthFrom) : null}
-name="selectedMonthFrom"
-format="MM/yyyy"
-className="form-control"
-onChange= {handleMonthFromChange}
-disableClock={true}
-calendarIcon={<FaCalendarAlt />}
-/>
-</Form.Group>
-<Form.Group>
-<Form.Label>Date to</Form.Label>
-<DateTimePicker
-value={selectedMonthTo ? new Date(2023, selectedMonthTo) : null}
-name="selectedMonthTo"
-format="MM/yyyy"
-className="form-control"
-onChange={handleMonthToChange}
-disableClock={true}
-calendarIcon={<FaCalendarAlt />}
-/>
+    <Container>
+      <Row className="table-filter">
+        
+          <Form.Label>Doughnut Chart</Form.Label>
 
-</Form.Group>
-<Col className='table-filter--size'>
-<Row className="form-buttons-container">
-<Button onClick={handleClearMonthFilter}>
-Clear date filter
-</Button>
-
-</Row>
-</Col>
-</Row>
-</Col>
-</Row>
-
-<Doughnut data={dataDoughnut} />  
-<Row className='table-filter'>
-<Col className='table-filter--size'>
-</Col>
-</Row>
-
-</Container>
+          <Row className="table-filter">
+            <Col className="table-filter--size">
+              <Form.Group>
+                <Col>
+                  <Form.Label>Date from:</Form.Label>
+                  <DateTimePicker
+                    value={startDate}
+                    name="startDate"
+                    format="yyyy-MM-dd"
+                    className="table-filter--date"
+                    onChange={handleStartDateChange}
+                    disableClock={true}
+                    calendarIcon={<FaCalendarAlt />}
+                  />
+                </Col>
+              </Form.Group>
+            </Col>
+            </Row>
+            <Row className="table-filter">
+            <Col className="table-filter--size">
+              <Form.Group>
+                <Col>
+                  <Form.Label>Date to:</Form.Label>
+                  <DateTimePicker
+                    value={endDate}
+                    name="endDate"
+                    format="yyyy-MM-dd"
+                    className="table-filter--date"
+                    onChange={handleEndDateChange}
+                    disableClock={true}
+                    calendarIcon={<FaCalendarAlt />}
+                  />
+                </Col>
+              </Form.Group>
+            </Col>
+          </Row>
+          
+        
+      </Row>
+      {!expenses&&<h4>Data not exist, wrong date format</h4>}
+      <Doughnut data={dataDoughnut} />
+    </Container>
   );
 }
 export default DoughnutChart;
--- money-maven-web/src/components/ExpenseTable.jsx
+++ money-maven-web/src/components/ExpenseTable.jsx
@@ -1,26 +1,32 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Button, Container, Pagination, Table, Form, Row, Col } from 'react-bootstrap';
-import Expense from './Expense';
-import NoElementsTableRow from './NoElementsTableRow';
-import { Link } from 'react-router-dom';
-import { FaCalendarAlt } from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Button,
+  Container,
+  Pagination,
+  Table,
+  Form,
+  Row,
+  Col,
+} from "react-bootstrap";
+import Expense from "./Expense";
+import NoElementsTableRow from "./NoElementsTableRow";
+import { Link } from "react-router-dom";
+import { FaCalendarAlt } from "react-icons/fa";
 import DateTimePicker from "react-datetime-picker";
-import moment from 'moment';
-import DoughnutChart from './DoughnutChart/'
+import moment from "moment";
 
 function ExpenseTable() {
   const [expenses, setExpenses] = useState([]);
   const [expenseTypes, setExpenseTypes] = useState([]);
   const [deleteExpense, setDeleteExpense] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(0)
+  const [totalPages, setTotalPages] = useState(0);
   const [pageSize, setPageSize] = useState(10);
-  const [sortDirection, setSortDirection] = useState('DESC');
-  const [selectedExpenseType, setSelectedExpenseType] = useState('');
+  const [sortDirection, setSortDirection] = useState("DESC");
+  const [selectedExpenseType, setSelectedExpenseType] = useState("");
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-  
 
   useEffect(() => {
     axios
@@ -31,15 +37,15 @@ function ExpenseTable() {
           direction: sortDirection,
           expenseTypeName: selectedExpenseType || null,
           startDate: startDate || null,
-          endDate: endDate|| null
-        }
+          endDate: endDate || null,
+        },
       })
       .then((response) => {
-        if(response.status === 200) {
+        if (response.status === 200) {
           setExpenses(response.data.content);
           setTotalPages(response.data.totalPages);
         }
-        if(response.status === 204) {
+        if (response.status === 204) {
           setExpenses([]);
           setTotalPages(1);
           setCurrentPage(0);
@@ -47,131 +53,158 @@ function ExpenseTable() {
       })
       .catch((err) => console.log(err));
 
-      axios // added axios call to get expense types
+    axios // added axios call to get expense types
       .get("http://localhost:8080/api/expenseTypes")
       .then((response) => setExpenseTypes(response.data))
       .catch((err) => console.log(err));
-
-  }, [currentPage, pageSize, sortDirection, deleteExpense, selectedExpenseType, startDate, endDate]);
+  }, [
+    currentPage,
+    pageSize,
+    sortDirection,
+    deleteExpense,
+    selectedExpenseType,
+    startDate,
+    endDate,
+  ]);
 
   const handlePageSizeChange = (e) => {
     setPageSize(e.target.value);
-    setCurrentPage(0)
-  }
+    setCurrentPage(0);
+  };
   const handleSortDirectionChange = () => {
-    sortDirection === 'ASC' ? setSortDirection('DESC') : setSortDirection ('ASC');
-  }
+    sortDirection === "ASC"
+      ? setSortDirection("DESC")
+      : setSortDirection("ASC");
+  };
 
   const handleSelectedExpenseTypeChange = (e) => {
     setSelectedExpenseType(e.target.value);
-  }
+  };
 
   const handleStartDateChange = (e) => {
-    if(e != null){
-      setStartDate(moment(e).format('YYYY-MM-DDTHH:mm:ss'));
+    if (e != null) {
+      setStartDate(moment(e).format("YYYY-MM-DDTHH:mm:ss"));
     } else {
       setStartDate(e);
     }
-  }
-  
+  };
+
   const handleEndDateChange = (e) => {
-    if(e != null){
-      setEndDate(moment(e).format('YYYY-MM-DDTHH:mm:ss'));
+    if (e != null) {
+      setEndDate(moment(e).format("YYYY-MM-DDTHH:mm:ss"));
     } else {
       setEndDate(e);
     }
-  }
-
+  };
 
   let expenseTypeJsx;
 
-  if(expenseTypes.length > 0){
+  if (expenseTypes.length > 0) {
     expenseTypeJsx = expenseTypes.map((expenseType) => {
-      return (<option key={expenseType.typeId} value={expenseType.typeName}>{expenseType.typeName}</option>)
-    })
+      return (
+        <option key={expenseType.typeId} value={expenseType.typeName}>
+          {expenseType.typeName}
+        </option>
+      );
+    });
   }
 
   let expensesJsx;
-  if(expenses.length > 0){
+  if (expenses.length > 0) {
     expensesJsx = expenses.map((expense) => {
-      return (<Expense expense = {expense} onDelete={() => setDeleteExpense(Date.now())} key={expense.expenseId}/>)
+      return (
+        <Expense
+          expense={expense}
+          onDelete={() => setDeleteExpense(Date.now())}
+          key={expense.expenseId}
+        />
+      );
     });
   } else {
-    expensesJsx = <NoElementsTableRow elementType={"Expenses"}/>;
+    expensesJsx = <NoElementsTableRow elementType={"Expenses"} />;
   }
-  
+
   return (
     <>
       <Container>
-        <Row className='table-filter'>
-            <Col>
-              <Form.Group>
-                <Form.Select
-                  id="expenseTypeSelect"
-                  value={selectedExpenseType}
-                  className='expense-type--select'
-                  onChange={handleSelectedExpenseTypeChange}
-                >
-                  <option value=''>Filter by</option>
-                  {expenseTypeJsx}
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Col className='table-filter--size'>
-                <Form.Group as={Row}>
-                    <Form.Label column>Page size:</Form.Label>
-                    <Col>
-                        <Form.Select 
-                        id="pageSizeSelect" 
-                        value={pageSize} 
-                        onChange={handlePageSizeChange}
-                        className='size--select'>
-                            <option value = "10">10</option>
-                            <option value = "15">15</option>
-                            <option value = "20">20</option>
-                        </Form.Select>
-                    </Col>
-                </Form.Group>
-            </Col>
-            <Col className='table-filter--sort'>
-                <Form.Group className='form-style'>
-                    <Button className="mx-2" variant="primary" onClick={handleSortDirectionChange}>
-                        {sortDirection === 'ASC' ? <>oldest to newest</> : <>newest to oldest</>}
-                    </Button>
-                </Form.Group>
-            </Col>
-        </Row>
-        <Row className='table-filter'>
+        <Row className="table-filter">
           <Col>
-          <Form.Label>Date from:</Form.Label>
-          <DateTimePicker
-                  value={startDate}
-                  name="startDate"
-                  format="yyyy-MM-dd"
-                  calendarIcon={<FaCalendarAlt />}
-                  className='table-filter--date'
-                  disableClock={true}
-                  yearPlaceholder="YYYY"
-                  monthPlaceholder="MM"
-                  dayPlaceholder="DD"
-                  onChange={handleStartDateChange}
-                />
+            <Form.Group>
+              <Form.Select
+                id="expenseTypeSelect"
+                value={selectedExpenseType}
+                className="expense-type--select"
+                onChange={handleSelectedExpenseTypeChange}
+              >
+                <option value="">Filter by</option>
+                {expenseTypeJsx}
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col className="table-filter--size">
+            <Form.Group as={Row}>
+              <Form.Label column>Page size:</Form.Label>
+              <Col>
+                <Form.Select
+                  id="pageSizeSelect"
+                  value={pageSize}
+                  onChange={handlePageSizeChange}
+                  className="size--select"
+                >
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                </Form.Select>
+              </Col>
+            </Form.Group>
+          </Col>
+          <Col className="table-filter--sort">
+            <Form.Group className="form-style">
+              <Button
+                className="mx-2"
+                variant="primary"
+                onClick={handleSortDirectionChange}
+              >
+                {sortDirection === "ASC" ? (
+                  <>oldest to newest</>
+                ) : (
+                  <>newest to oldest</>
+                )}
+              </Button>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className="table-filter">
+          <Col>
+            <Form.Label>Date from:</Form.Label>
+            <DateTimePicker
+              value={startDate}
+              name="startDate"
+              format="yyyy-MM-dd"
+              calendarIcon={<FaCalendarAlt />}
+              className="table-filter--date"
+              disableClock={true}
+              yearPlaceholder="YYYY"
+              monthPlaceholder="MM"
+              dayPlaceholder="DD"
+              onChange={handleStartDateChange}
+            />
           </Col>
           <Col>
-          <Form.Label>Date until:</Form.Label>
+            <Form.Label>Date until:</Form.Label>
             <DateTimePicker
-                  value={endDate}
-                  name="dateFilterUntil"
-                  format="yyyy-MM-dd"
-                  calendarIcon={<FaCalendarAlt />}
-                  className='table-filter--date'
-                  disableClock={true}
-                  yearPlaceholder="YYYY"
-                  monthPlaceholder="MM"
-                  dayPlaceholder="DD"
-                  onChange={handleEndDateChange}
-                />
-            </Col>
+              value={endDate}
+              name="dateFilterUntil"
+              format="yyyy-MM-dd"
+              calendarIcon={<FaCalendarAlt />}
+              className="table-filter--date"
+              disableClock={true}
+              yearPlaceholder="YYYY"
+              monthPlaceholder="MM"
+              dayPlaceholder="DD"
+              onChange={handleEndDateChange}
+            />
+          </Col>
         </Row>
         <Table>
           <thead>
@@ -187,20 +220,29 @@ function ExpenseTable() {
           <tbody>{expensesJsx}</tbody>
         </Table>
         <Pagination>
-            <Pagination.First onClick={() => setCurrentPage(0)} />
-            <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0} />
-            {[...Array(totalPages)].map((page, index) => (
-                <Pagination.Item key={index} active={index === currentPage} onClick={() => setCurrentPage(index)}>
-                    {index + 1}
-                </Pagination.Item>
-            ))}
-            <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages - 1} />
-            <Pagination.Last onClick={() => setCurrentPage(totalPages - 1)} />
+          <Pagination.First onClick={() => setCurrentPage(0)} />
+          <Pagination.Prev
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 0}
+          />
+          {[...Array(totalPages)].map((page, index) => (
+            <Pagination.Item
+              key={index}
+              active={index === currentPage}
+              onClick={() => setCurrentPage(index)}
+            >
+              {index + 1}
+            </Pagination.Item>
+          ))}
+          <Pagination.Next
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={currentPage === totalPages - 1}
+          />
+          <Pagination.Last onClick={() => setCurrentPage(totalPages - 1)} />
         </Pagination>
         <Link to={"/expense/create"} className="form-style">
           <Button variant="primary">Create new</Button>
         </Link>
-        <DoughnutChart /> 
       </Container>
     </>
   );
