3ea92a41c94cdcdcf9d7818ecf7d13933a8ea4a5
Egidijus Jakavonis <e.jakavonis@gmail.com>
2023-05-09 17:35:13 +0300

    Line chart add

--- money-maven-web/src/components/LineChart.jsx
+++ money-maven-web/src/components/LineChart.jsx
@@ -12,22 +12,16 @@ import {
 } from "chart.js";
 import DateTimePicker from "react-datetime-picker";
 import { FaCalendarAlt } from "react-icons/fa";
-import { Container, Row, Form, Col } from "react-bootstrap";
+import { Container, Row, Form, Col, Table } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import moment from "moment";
-import Chart from "chart.js/auto";
-
-Chart.register("myCustomChartType", {
-  // implementation goes here
-});
 
 ChartJs.register(
   CategoryScale,
   LinearScale,
   PointElement,
   LineElement,
-
   Title,
   Tooltip,
   Legend,
@@ -35,18 +29,18 @@ ChartJs.register(
 );
 
 function LineChart() {
-  const [incomes, setIncomes] = useState([]);
   const [expenses, setExpenses] = useState([]);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
-  const [deleteIncome] = useState([]);
-  const [deleteExpense] = useState([]);
-  const [selectedStartDate] = useState("");
-  const [selectedSetEndDate] = useState("");
+  const [deleteExpense, setDeleteExpense] = useState(false);
 
   useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("user"));
     axios
       .get("http://localhost:8080/api/expenses?page=0&pageSize=10000", {
+        headers: {
+          Authorization: `Bearer ${token.accessToken}`,
+        },
         params: {
           startDate: startDate || null,
           endDate: endDate || null,
@@ -57,27 +51,9 @@ function LineChart() {
         setExpenses(response.data.content);
       })
       .catch((err) => console.log(err));
-    axios
-      .get("http://localhost:8080/api/incomes?page=0&pageSize=10000", {
-        params: {
-          startDate: startDate || null,
-          endDate: endDate || null,
-        },
-      })
-      .then((response) => {
-        console.log("Income data received:", response.data.conten);
-        setIncomes(response.data.content);
-      })
-      .catch((err) => console.log(err));
-  }, [startDate, endDate, deleteIncome, deleteExpense]);
+  }, [startDate, endDate, deleteExpense]);
 
   const arrayExpenses = expenses
-    ? Array.from(new Set(expenses.map((expense) => expense.expenseTypeName)))
-    : [];
-  const arrayIncomes = incomes
-    ? Array.from(new Set(incomes.map((income) => income.incomeAmount)))
-    : [];
-  const categories = expenses
     ? Array.from(new Set(expenses.map((expense) => expense.expenseTypeName)))
     : [];
 
@@ -90,7 +66,7 @@ function LineChart() {
     if (e != null) {
       setStartDate(moment(e).format("YYYY-MM-DDTHH:mm:ss"));
     } else {
-      setStartDate(e);
+      setStartDate(null);
     }
   };
 
@@ -98,150 +74,98 @@ function LineChart() {
     if (e != null) {
       setEndDate(moment(e).format("YYYY-MM-DDTHH:mm:ss"));
     } else {
-      setEndDate(e);
+      setEndDate(null);
     }
   };
 
-  // const getLabels = (startDate, endDate) => {
-  //   const start = moment(startDate).startOf("month");
-  //   const end = moment(endDate).endOf("month");
-  //   const numMonths = end.diff(start, "months") + 1;
-  //   return Array(numMonths).fill().map((_, i) => start.clone().add(i, "months").format("MMM"));
-  // };
-
-  const getLabels = (startDate, numMonths) => {
+  const getLabels = (startDate, endDate) => {
     const start = startDate ? moment(startDate) : moment().startOf("year");
-    const end = moment(endDate).endOf("month");
+    const end = endDate
+      ? moment(endDate).endOf("month")
+      : moment().endOf("month");
+    const numMonths = end.diff(start, "months") + 1;
     return Array(numMonths)
       .fill()
-      .map((_, i) => start.add(i, "months").format("MMM"));
+      .map((_, i) => start.clone().add(i, "months").format("MMM"));
   };
 
   const dataLine = {
-    labels: getLabels(startDate, 6),
+    labels: getLabels(startDate, endDate),
     datasets: [
       {
         type: "line",
         label: "Expenses",
         backgroundColor: "red",
         tension: 0.3,
-        data:
-          arrayExpenses &&
-          arrayExpenses.map((category) =>
-            expenses
-              .filter(
-                (expense) =>
-                  expense.expenseTypeName === category &&
-                  (!selectedStartDate ||
-                    new Date(expense.expenseDatetime) >=
-                      new Date(selectedStartDate)) &&
-                  (!selectedSetEndDate ||
-                    new Date(expense.expenseDatetime) <=
-                      new Date(selectedSetEndDate))
-              )
-              .reduce((acc, expense) => acc + expense.expenseAmount, 0)
-          ),
-      },
-
-      {
-        labels: categories,
-
-        type: "bar",
-        label: "Expenses by category",
-        data:
-          categories &&
-          categories.map((category) =>
-            expenses
-              .filter(
-                (expense) =>
-                  expense.expenseTypeName === category &&
-                  (!selectedStartDate ||
-                    new Date(expense.expenseDatetime) >=
-                      new Date(selectedStartDate)) &&
-                  (!selectedSetEndDate ||
-                    new Date(expense.expenseDatetime) <=
-                      new Date(selectedSetEndDate))
-              )
-              .reduce((acc, expense) => acc + expense.expenseAmount, 0)
-          ),
-        backgroundColor: ["#36a2eb"],
-      },
-      {
-        label: "Income",
-        backgroundColor: "green",
-        tension: 0.2,
-        data:
-          arrayIncomes &&
-          arrayIncomes.map((incomeId) =>
-            incomes
-              .filter(
-                (income) =>
-                  income.incomeId === incomeId &&
-                  (!selectedStartDate ||
-                    new Date(income.incomeDatetime) >=
-                      new Date(selectedStartDate)) &&
-                  (!selectedSetEndDate ||
-                    new Date(income.incomeDatetime) <=
-                      new Date(selectedSetEndDate))
-              )
-              .reduce((acc, income) => acc + income.incomeAmount, 0)
-          ),
+        data: expenses.map(
+          (selectedExpenses) => selectedExpenses.expenseAmount
+        ),
       },
     ],
   };
-
   return (
     <Container>
       <Form.Label>Line Chart</Form.Label>
-      <Row className="table-cell">
-        <Row className="table-filter">
-          <Col className="table-filter--size">
-            <Form.Group>
-              <Col>
-                <Form.Label>Date from:</Form.Label>
-                <DateTimePicker
-                  value={startDate}
-                  name="startDate"
-                  format="yyyy-MM-dd"
-                  className="table-filter--date"
-                  onChange={handleStartDateChange}
-                  disableClock={true}
-                  calendarIcon={<FaCalendarAlt />}
-                />
-              </Col>
-            </Form.Group>
-          </Col>
+      <Row className="row justify-center">
+        <Row className="table-cell">
+          <Row className="table-filter">
+            <Col className="table-filter--size">
+              <Form.Group>
+                <Col>
+                  <Form.Label>Date from:</Form.Label>
+                  <DateTimePicker
+                    value={startDate}
+                    name="startDate"
+                    format="yyyy-MM-dd"
+                    className="table-filter--date"
+                    onChange={handleStartDateChange}
+                    disableClock={true}
+                    calendarIcon={<FaCalendarAlt />}
+                  />
+                </Col>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="table-filter">
+            <Col className="table-filter--size">
+              <Form.Group>
+                <Col>
+                  <Form.Label>Date until:</Form.Label>
+                  <DateTimePicker
+                    value={endDate}
+                    name="endDate"
+                    format="yyyy-MM-dd"
+                    onChange={handleEndDateChange}
+                    disableClock={true}
+                    calendarIcon={<FaCalendarAlt />}
+                    isInvalid={
+                      startDate &&
+                      endDate &&
+                      moment(startDate).isAfter(moment(endDate))
+                        ? "is-invalid"
+                        : "Start date cannot be after end date"
+                    }
+                    className="table-filter--date"
+                  />
+                </Col>
+              </Form.Group>
+            </Col>
+          </Row>
         </Row>
-        <Row className="table-filter">
-          <Col className="table-filter--size">
-            <Form.Group>
-              <Col>
-                <Form.Label>Date to:</Form.Label>
-                <DateTimePicker
-                  value={endDate}
-                  name="endDate"
-                  format="yyyy-MM-dd"
-                  className="table-filter--date"
-                  onChange={handleEndDateChange}
-                  disableClock={true}
-                  calendarIcon={<FaCalendarAlt />}
-                />
-              </Col>
-            </Form.Group>
-          </Col>
-        </Row>
+        {!expenses && <tr>Data not exist, wrong date format</tr>}
+        <Line data={dataLine} />
+        <Table class="table table">
+          <tbody>
+            <td class="table-active">
+              <Row className="table-filter--date">
+                <td class="table-Secondary">
+                  Selected period expenses : € {selectedExpenses}
+                </td>
+              </Row>
+            </td>
+          </tbody>
+        </Table>
       </Row>
-      {!expenses && <h4>Data not exist, wrong date format</h4>}
-      <Line data={dataLine} />
-      <Form.Group>
-        <Row className="table-row">
-          <Form.Label>
-            Selected period Expenses : € {selectedExpenses}
-          </Form.Label>
-
-          <Form.Label>Current year Expenses :</Form.Label>
-        </Row>
-      </Form.Group>
     </Container>
   );
 }
